e549294c771a2eb4f460f46990becf0ffa0b7eca
Paulo Barros <697817+paulobarros@users.noreply.github.com>
2023-02-13 16:04:24 -0300

    Add dataset and doc

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' Brazilian Amphibian Species List Dataset
+#'
+#' A dataset of the official Brazilian Amphibian Species List publish
+#' by the Brazilian Herpetolgical Society (SBH)
+#'
+#' @format ## `amphibiansBR`
+#' A data frame with 1,188 rows and 8 columns:
+#' \describe{
+#'   \item{order}{Taxonomic Order}
+#'   \item{family}{Taxonomic Family}
+#'   \item{subfamily}{Taxonomic Family}
+#'   \item{sp_num}{Species number on list}
+#'   \item{genus}{Genus}
+#'   \item{epithet}{Specifc epithet}
+#'   \item{author}{Authorship of original description}
+#'   \item{status}{General remarks about species}
+#'   ...
+#' }
+#' @source <https://zenodo.org/record/4716176>
+"amphibiansBR"
